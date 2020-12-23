201a0118be5227ec1330b6c1431b08007fabbc99
jiang1991 <ji.ang@outlook.com>
2020-12-23 10:25:04 +0800

    0.1.0 发布版 在线升级

--- app/src/main/java/com/lepu/nordicble/activity/AgreementActivity.kt
+++ app/src/main/java/com/lepu/nordicble/activity/AgreementActivity.kt
@@ -3,6 +3,8 @@ package com.lepu.nordicble.activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import com.lepu.nordicble.BuildConfig
 import com.lepu.nordicble.R
 import com.lepu.nordicble.utils.readAgreementConfig
 import com.lepu.nordicble.utils.saveAgreementConfig
@@ -18,12 +20,23 @@ class AgreementActivity : AppCompatActivity() {
 
     private fun initUI() {
         agreement.loadUrl("file:///android_asset/agreement.html")
+
         agree.setOnClickListener {
-            saveAgreementConfig(this, true)
-            jumpToNext()
+
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("用户协议")
+            builder.setMessage("我已阅读并同意乐普安心宝APP用户服务协议")
+            builder.setPositiveButton("确认") {_, _ ->
+                saveAgreementConfig(this, true)
+                jumpToNext()
+            }
+            builder.setNegativeButton("取消") {dialog, _ ->
+                dialog.dismiss()
+            }
+            builder.show()
         }
 
-        if (getString(R.string.key_need_agreement) == "false") {
+        if (BuildConfig.FLAVOR != "anxin") {
             jumpToNext()
         }
 
